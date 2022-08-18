f86f7f511ababdc8dfc9507f0c9451f0eae10391
Super <chuangsuper@gmail.com>
2022-08-18 17:15:12 +0800

    Update random_prize.js

--- pdf/random_prize.js
+++ pdf/random_prize.js
@@ -4,5 +4,5 @@ let thePrize = Math.round(Math.random()*prizeNum)
 document.write("<h3>"+prizeList[thePrize]+"</h3>")
 function pay(){
     alert("您需要付費才能繼續抽獎!點擊確認前往付款!")
-    location.href = "../pay"
+    location.href = "https://super1115.github.io/Lottery/pay/index.html"
 }
