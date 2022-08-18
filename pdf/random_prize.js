350b98c663a0d21cedda6d277a238d5bd3c33e96
Super <chuangsuper@gmail.com>
2022-08-18 17:06:28 +0800

    Upload Files

---
+++ pdf/random_prize.js
@@ -0,0 +1,8 @@
+var prizeList =["一隻企鵝","下次會更好","一頭大象","下次會更好"]
+let prizeNum = prizeList.length-1
+let thePrize = Math.round(Math.random()*prizeNum)
+document.write("<h3>"+prizeList[thePrize]+"</h3>")
+function pay(){
+    alert("您需要付費才能繼續抽獎!點擊確認前往付款!")
+    location.href = "../pay"
+}
